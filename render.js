25b4ba80d8faccc1b99bb43a28765b96d3e11b4d
Sergey Royz <zjor.se@gmail.com>
2019-05-22 14:17:03 +0200

    watching file changes

--- render.js
+++ render.js
@@ -2,12 +2,30 @@ const fs = require('fs')
 const jsyaml = require('js-yaml')
 const Handlebars = require('handlebars')
 
-const template = Handlebars.compile(fs.readFileSync('template.hbs', 'utf8'))
-const data = jsyaml.load(fs.readFileSync('data.yaml', 'utf8'))
+const templateFilename = './src/template.hbs'
+const dataFilename = './src/data.yaml'
+const outputFilename = './index.html'
 
-console.log('Rendering template')
-const html = template(data) 
-console.log(html)
+const shouldWatch = (args) => args.filter(x => x == '--watch').length > 0
 
-fs.writeFileSync('index.html', html)
-console.log(`${html.length} bytes written`)
+const render = (templateFilename, dataFilename, outputFilename) => {
+    const template = Handlebars.compile(fs.readFileSync(templateFilename, 'utf8'))
+    const data = jsyaml.load(fs.readFileSync(dataFilename, 'utf8'))
+        
+    console.log('Rendering template')
+    const html = template(data) 
+    console.log(html)
+    
+    fs.writeFileSync(outputFilename, html)
+    console.log(`${html.length} bytes written`)    
+}
+
+render(templateFilename, dataFilename, outputFilename)
+
+if (shouldWatch(process.argv)) {
+    console.log("Watching... Press Ctrl+C to quit.")
+    const watcher = fs.watch('./src', (event, filename) => {
+        console.log(`${event}: ${filename}`)
+        render(templateFilename, dataFilename, outputFilename)
+    })
+}
